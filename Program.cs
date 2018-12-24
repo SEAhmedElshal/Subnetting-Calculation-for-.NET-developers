1b57c2709e987e824b1a78cc52f9b62bb4fa7b56
Ahmed Elshal <40341302+SEAhmedElshal@users.noreply.github.com>
2018-12-24 16:41:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPNetwork IPnetwork = IPNetwork.Parse("100.100.100.10/19");
+            Console.WriteLine("Network ID : {0}", IPnetwork.Network);
+            Console.WriteLine("Custom Subnet Mask : {0}", IPnetwork.Netmask);
+            Console.WriteLine("Broadcast IP : {0}", IPnetwork.Broadcast);
+            Console.WriteLine("First Valid IP: {0}", IPnetwork.FirstUsable);
+            Console.WriteLine("Last Valid IP : {0}", IPnetwork.LastUsable);
+            Console.WriteLine("Usable : {0}", IPnetwork.Usable);
+            Console.WriteLine("Total : {0}", IPnetwork.Total);
+            Console.WriteLine("CIDR : {0}", IPnetwork.Cidr);
+            Console.ReadKey();
+        }
+    }
+}
